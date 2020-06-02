f73c32bbb26f5abcca0e41d52702dbc5642e3186
DaoVanThang <captionnemo2701@users.noreply.github.com>
2020-06-03 02:06:12 +0700

    Create frm_Note.cs

---
+++ SimpleNote/SimpleNote/Views/frm_Note.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SimpleNote.Views;
+
+namespace SimpleNote.Views
+{
+    public partial class frm_Note : Form
+    {
+        public frm_Note()
+        {
+            InitializeComponent();
+        }
+    }
+}
