c8eede03bed278c3d32aff544bb58fb0d8553c87
DaoVanThang <captionnemo2701@users.noreply.github.com>
2020-06-05 22:11:30 +0700

    Update frm_Note.cs

--- SimpleNote/SimpleNote/Views/frm_Note.cs
+++ SimpleNote/SimpleNote/Views/frm_Note.cs
@@ -17,5 +17,18 @@ namespace SimpleNote.Views
         {
             InitializeComponent();
         }
+
+        private void frm_Note_Load(object sender, EventArgs e)
+        {
+            foreach(FontFamily font in FontFamily.Families)
+            {
+                this.comboBox_TextStyle.Items.Add(font.Name.ToString());
+            }    
+        }
+
+        private void comboBox_TextStyle_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            this.richText_Note.Font = new Font(this.comboBox_TextStyle.Font, this.richText_Note.Font.Style);
+        }
     }
 }
